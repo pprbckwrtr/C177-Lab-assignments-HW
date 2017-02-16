3af696e0a9cc4c74445e490563edb45a47f095e4
GAYANE <paperbackwriter@g.ucla.edu>
2017-02-15 19:04:10 -0800

    in-class portion of lab work

---
+++ exercise-6/my-first-script.py
@@ -0,0 +1,4 @@
+print ("Hello world! I'm using python!")
+var1= 2+3
+print ("var1 is " + var1)
+
---
+++ exercise-6/name_fav.py
@@ -0,0 +1,16 @@
+# define variables
+name = "Gaurav"
+fav_fruit = "oranges"
+
+# print a sentence using the variables
+
+
+# import in the sys module that contains the function argv
+import sys
+
+# define variables
+name = sys.argv[1]
+fav_fruit = sys.argv[2]
+
+# print a sentence using the variables
+print("Hi. My name is "+ name+ ", I like " + fav_fruit)
